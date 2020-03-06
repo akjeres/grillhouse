d9519417fdbc3a51230e82c16cad231336c4894b
akjeres <akjeres@gmail.com>
2020-03-07 01:05:06 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.');
+
+http.createServer(function(req, res) {
+    file.serve(req, res);
+}).listen(9000);
+
+console.log('Server running on port 9000');
